5bc6de9d573ff4e7ba8aaf51c2c8320745628102
rrbagautdinov <wh1sperdnb@mail.ru>
2020-07-06 13:17:22 +0300

    Поправлен код, на соответствие заданию

--- src/main/java/ru/geekbrains/lessons2/lesson5/MainClass.java
+++ src/main/java/ru/geekbrains/lessons2/lesson5/MainClass.java
@@ -10,12 +10,15 @@ public class MainClass {
 
     public static void createArrayWithoutThreads(int size) {
         System.out.println("Создаем массив из " + size + " элементов на 1 потоке");
-        long startTime = System.currentTimeMillis();
+
         int[] array = new int[size];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = 1;
         }
+
+        long startTime = System.currentTimeMillis();
+
         for (int i = 0; i < array.length; i++) {
             array[i] = (int)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
@@ -26,9 +29,13 @@ public class MainClass {
 
     public static void createArrayWithThreads(int size) throws InterruptedException {
         System.out.println("Создаем массив из " + size + " элементов на 2 потоках");
-        long startTime = System.currentTimeMillis();
 
         int[] generalArray = new int[size];
+        for (int i = 0; i < generalArray.length; i++) {
+            generalArray[i] = 1;
+        }
+
+        long startTime = System.currentTimeMillis();
 
         int[] firstArray = Arrays.copyOfRange(generalArray, 0, generalArray.length/2);
         int[] secondArray = Arrays.copyOfRange(generalArray, generalArray.length/2, generalArray.length);
@@ -37,7 +44,6 @@ public class MainClass {
             @Override
             public synchronized void run() {
                 for (int i = 0; i < firstArray.length; i++) {
-                    firstArray[i] = 1;
                     firstArray[i] = (int)(firstArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
             }
@@ -48,7 +54,6 @@ public class MainClass {
             @Override
             public synchronized void run() {
                 for (int i = 0; i < secondArray.length; i++) {
-                    secondArray[i] = 1;
                     secondArray[i] = (int)(secondArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
             }
@@ -59,6 +64,7 @@ public class MainClass {
         thread2.join();
 
         int[] sumArray = new int[firstArray.length + secondArray.length];
+
         long timeSpent = System.currentTimeMillis() - startTime;
         System.out.println("Программа выполнялась " + timeSpent + " миллисекунд");
         System.out.println();
