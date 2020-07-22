d3a7d972db10ddd6f57399bd26e8c8c1441ce949
rrbagautdinov <wh1sperdnb@mail.ru>
2020-07-22 12:47:44 +0300

    ДЗ №15-16 выполнено

---
+++ src/main/java/ru/geekbrains/lessons2/lesson7/server/AuthService.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.lessons2.lesson7.server;
+
+import java.sql.*;
+
+public class AuthService {
+
+    private static Connection connection;
+    private static Statement stmt;
+
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+            stmt = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getNickByLoginAndPass(String login, String pass) {
+        String sql = String.format("select nickname from main where login = '%s' and password = '%s'", login, pass);
+        try {
+           ResultSet rs = stmt.executeQuery(sql);
+           if (rs.next()) {
+               return rs.getString(1);
+           }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
