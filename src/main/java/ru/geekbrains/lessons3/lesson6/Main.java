c031473a65347bbec17ba8f24abd5a8a1f83307a
rrbagautdinov <65827549+rrbagautdinov@users.noreply.github.com>
2020-08-08 18:38:11 +0300

    Залив кода 2

--- src/main/java/ru/geekbrains/lessons3/lesson6/Main.java
+++ src/main/java/ru/geekbrains/lessons3/lesson6/Main.java
@@ -1,13 +1,60 @@
 package ru.geekbrains.lessons3.lesson6;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        lesson1(new int[]{1, 4, 3, 4, 5, 6, 4, 8, 9, 10});
+        //task1(new int[]{1, 4, 3, 2, 4, 6, 4, 8, 9, 10});
+        task2(new int[]{1, 4, 1, 4, 1, 4});
     }
 
-    public static void lesson1(int[] massive) {
+    public static void task1(int[] massive) {
+        int findFour = 0;
+        int lastFour = 0;
         for (int i = 0; i < massive.length; i++) {
-            System.out.println(massive[i]);
+            if (massive[i] == 4) {
+                findFour++;
+            }
+        }
+        if (findFour == 0) {
+            throw new RuntimeException();
+        }
+
+        for (int i = massive.length - 1; i >= 0; i--) {
+            if (massive[i] == 4) {
+                lastFour = i;
+                break;
+            }
+        }
+
+        System.out.println("В массиве найдено четверок: " + findFour + " штуки");
+        System.out.println("Индекс последней четверки: " + lastFour);
+        System.out.println("Вытаскиваю остаточный массив, после последней четверки");
+        int massive2[] = Arrays.copyOfRange(massive, lastFour + 1, massive.length);
+        for (int i = 0; i < massive2.length; i++) {
+            System.out.print(massive2[i] + " ");
+        }
+    }
+
+    public static boolean task2(int[] massiv) {
+        int countOne = 0;
+        int countFour = 0;
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i] == 1) {
+                countOne++;
+            } else if( massiv[i] == 4) {
+                countFour++;
+            }
+        }
+
+        if (countOne == 0) {
+            return false;
+        } else if (countFour == 0) {
+            return false;
+        } else if (countOne == 0 && countFour == 0) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
